a5d0766c724b9352b5da205016bbe6cd883b90b3
ChoEun-Sang <wh94es@gmail.com>
2023-10-12 22:42:02 +0900

    feat: 회원 목록 조회 api 추가

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,5 +1,6 @@
 import { userRequest, authRequest } from "@/api";
 
+// 로그인
 interface Login {
   email: string;
   password: string;
@@ -7,6 +8,13 @@ interface Login {
 
 export const login = async (body: Login) => {
   const res = await authRequest.post("/admin/auth/login", body);
+
+  return res.data;
+};
+
+// 회원 목록 조회
+export const getUserList = async () => {
+  const res = await userRequest.get("/admin/member/list");
 
   return res.data;
 };
