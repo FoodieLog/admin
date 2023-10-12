c6d4bf2fe50bea1ba6b4ea2c1433b6a7101c2524
ChoEun-Sang <wh94es@gmail.com>
2023-10-12 22:44:08 +0900

    feat: 회원목록조회 페이지 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,59 @@
 "use client";
-import { USER_COLUMNS } from "@/constants/table";
-// import Row from "@/components/Table/Table";
-import DateCalendarServerRequest from "@/components/Calendar";
+import { useEffect, useState, ReactElement } from "react";
+import { USER_LIST_COLUMNS } from "@/constants/table";
+import { getUserList } from "@/api/request";
+import TableForm from "@/components/Table/TableForm";
+import Modal from "@/components/Modal";
+import TextArea from "antd/es/input/TextArea";
+
 export default function Home() {
-  const data = [
-    {
-      id: "feed_id",
-      type: "댓글/게시글",
-      link: "URL",
-      reported_id: "report_id",
-      email: "thesecon@gmail.com",
-      nickName: "HEROPY",
-      grade: "일반회원",
-      reporter: [
-        {
-          reporter_id: "reporter_id",
-          nickName: "신고자",
-          email: "repoter@gamil.com",
-          reason: "광고/욕설/음란/명예훼손/기타",
-          reportedAt: "2023-07-25 22:14",
-          processedAt: "2023-07-26 22:14",
-        },
-        {
-          reporter_id: "reporter_id",
-          nickName: "신고자",
-          email: "repoter@gamil.com",
-          reason: "광고/욕설/음란/명예훼손/기타",
-          reportedAt: "2023-07-25 22:14",
-          processedAt: "2023-07-26 22:14",
-        },
-      ],
-      status: "미처리/승인/반려",
-    },
-  ];
+  const [openModal, setOpenModal] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getUserListData();
+  }, []);
+
+  const getUserListData = async () => {
+    try {
+      const { response } = await getUserList();
+      setData(response.content);
+      console.log("유저데이터에러", response.content);
+    } catch (err) {
+      console.log("유저데이터에러", err);
+    }
+  };
+
+  const onClickBtn = () => {
+    setOpenModal(true);
+  };
 
   return (
-    <>
-      <DateCalendarServerRequest />
-    </>
+    <div className="flex flex-col gap-3">
+      {openModal && (
+        <Modal>
+          <div>
+            <h2>영구차단하시겠습니까?</h2>
+            <p>영구차단 사유를 작성해 주세요.</p>
+            <p>회원이 작성 사유를 확인할 수 있습니다.</p>
+            <TextArea />
+            <button type="button" onClick={() => setOpenModal(false)}>
+              취소
+            </button>
+            <button type="button" onClick={() => setOpenModal(false)}>
+              확인
+            </button>
+          </div>
+        </Modal>
+      )}
+      <p className="text-lg">회원 목록 조회</p>
+      <button
+        type="button"
+        onClick={onClickBtn}
+        className="px-5 py-2 bg-red-500 text-white rounded-md place-self-end"
+      >
+        영구 차단
+      </button>
+      <TableForm columns={USER_LIST_COLUMNS} data={data} />
+    </div>
   );
 }
