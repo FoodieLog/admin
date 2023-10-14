814d4e913964aebab3b36f9d9807411ed960221b
ChoEun-Sang <wh94es@gmail.com>
2023-10-14 23:15:20 +0900

    feat: 신고 리스트 조회 api 연결

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import { useEffect, useState } from "react";
+import TableForm from "@/components/Table/TableForm";
+import { getReportList } from "@/api/request";
+import { REPORT_LIST_COLUMNS } from "@/constants/table";
 
 function Report() {
-  return <div>Report</div>;
+  const [reportList, setReportList] = useState([]);
+
+  useEffect(() => {
+    getReportUsers();
+  }, []);
+
+  const getReportUsers = async () => {
+    const { response } = await getReportList();
+    setReportList(response.content);
+  };
+
+  return <TableForm data={reportList} columns={REPORT_LIST_COLUMNS} />;
 }
 
 export default Report;
