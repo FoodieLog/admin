7ab6fe6463ac7ddd0c501addd2c8d606ca92b854
ChoEun-Sang <wh94es@gmail.com>
2023-10-24 23:12:21 +0900

    feat: 신고 관리 페이지 - 모달 추가

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getReportList } from "@/api/request";
+import { getReportList, patchReport } from "@/api/request";
 import { REPORT_LIST_COLUMNS } from "@/constants/table";
 import TableForm from "@/components/Table/TableForm";
 import Buttom from "@/components/Buttom";
+import Modal from "@/components/Modal";
 
 function Report() {
+  const [openModal, setOpenModal] = useState(false);
   const [reportList, setReportList] = useState([]);
 
   useEffect(() => {
@@ -15,12 +17,25 @@ function Report() {
   const getReportUsers = async () => {
     const { response } = await getReportList();
     setReportList(response.content);
+    console.log(response.content);
   };
 
-  const onClickBtn = () => {};
+  // const patchReport = async () => {
+  //   await patchReport({contentId, reportedId, userStatus});
+
+  // }
+
+  const onClickBtn = () => {
+    setOpenModal(true);
+  };
 
   return (
     <div className="flex flex-col">
+      {openModal && (
+        <Modal>
+          <div></div>
+        </Modal>
+      )}
       <div className="mb-3 place-self-end">
         <Buttom onClick={onClickBtn} styles="bg-yellow-400" text="신고반려" />
         <Buttom onClick={onClickBtn} styles="ml-2 bg-red-500" text="신고승인" />
