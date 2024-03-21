7a5996ae44ca74700e9a5b1be6d59ab820d8f0d5
ChaDongmin <cdm1263@gmail.com>
2024-03-21 21:55:18 +0900

    refactor: data 구조분해할당

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,73 +1,80 @@
-import { userRequest, authRequest } from "@/api"
-import { BlockBody, PatchReportBody } from "@/types"
+import { userRequest, authRequest } from "@/api";
+import { BlockBody, PatchReportBody } from "@/types";
 
 // 로그인
 interface Login {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 export const login = async (body: Login) => {
-  const res = await authRequest.post("/admin/auth/login", body)
+  const { data } = await authRequest.post("/admin/auth/login", body);
 
-  return res.data
-}
+  return data;
+};
 
 // 회원 목록 조회
 export const getUserList = async () => {
-  const res = await userRequest.get("/admin/member/list")
+  const { data } = await userRequest.get("/admin/member/list");
 
-  return res.data
-}
+  return data;
+};
 
 // 신고 목록 조회
 export const getReportList = async () => {
-  const res = await userRequest.get("/admin/report/list")
+  const { data } = await userRequest.get("/admin/report/list");
 
-  return res.data
-}
+  return data;
+};
 
 // 신고 처리
 export const patchReport = async (body: PatchReportBody) => {
-  const res = await userRequest.patch("/admin/report/process", body)
-  return res.data
-}
+  const { data } = await userRequest.patch("/admin/report/process", body);
+  return data;
+};
 
 // 영구 정지
 export const postBlockMember = async (body: BlockBody) => {
-  const res = await userRequest.post("/admin/member/block", body)
+  const { data } = await userRequest.post("/admin/member/block", body);
 
-  return res.data
-}
+  return data;
+};
 
 // 탈퇴 회원 조회
 export const getWithdrawerList = async (badge: string, nickName?: string) => {
-  const res = await userRequest.get(
+  const { data } = await userRequest.get(
     `/admin/member/withdraw/list?badge=${badge}${
       nickName ? `&nickName=${nickName}` : ""
     }`
-  )
+  );
 
-  return res.data
-}
+  return data;
+};
 
 // 탈퇴 회원 복구
 export const patchRestoreWithdrawer = async (withdrawId: string) => {
-  const res = await userRequest.patch(
+  const { data } = await userRequest.patch(
     `/admin/member/withdraw/restore?withdrawId=${withdrawId}`
-  )
+  );
 
-  return res.data
-}
+  return data;
+};
 
 // 뱃지 처리 현황 조회
 export const getBadgeList = async (processedStatus: string) => {
-  const res = await userRequest.get(`/admin/member/badge/list?processedStatus=${processedStatus}`)
-  return res.data
-}
+  const { data } = await userRequest.get(
+    `/admin/member/badge/list?processedStatus=${processedStatus}`
+  );
+  return data;
+};
 
 // 뱃지 승인 반려 처리
-export const patchBadgeStatus = async (badgeApplyId: number, process: string) => {
-  const res = await userRequest.patch(`/admin/member/badge/${process}?badgeApplyId=${badgeApplyId}`);
-  return res.data;
+export const patchBadgeStatus = async (
+  badgeApplyId: number,
+  process: string
+) => {
+  const { data } = await userRequest.patch(
+    `/admin/member/badge/${process}?badgeApplyId=${badgeApplyId}`
+  );
+  return data;
 };
