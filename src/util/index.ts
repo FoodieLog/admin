7d0cf65be91cf4dbd001af0af9acc2c99e902625
ChaDongmin <cdm1263@gmail.com>
2024-04-04 07:04:49 +0900

    refactor: y-md 형식의 날짜 포맷을 y-m-d로 수정

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,10 +1,10 @@
 export const formatDate = (dateStr: string) => {
   const date = new Date(dateStr);
   const y = date.getFullYear();
-  const m = String(date.getMonth() + 1).padStart(2, '0');
-  const d = String(date.getDate()).padStart(2, '0');
-  const h = String(date.getHours()).padStart(2, '0');
-  const min = String(date.getMinutes()).padStart(2, '0');
-  const sec = String(date.getSeconds()).padStart(2, '0');
-  return `${y}-${m}${d} ${h}:${min}:${sec}`;
+  const m = String(date.getMonth() + 1).padStart(2, "0");
+  const d = String(date.getDate()).padStart(2, "0");
+  const h = String(date.getHours()).padStart(2, "0");
+  const min = String(date.getMinutes()).padStart(2, "0");
+  const sec = String(date.getSeconds()).padStart(2, "0");
+  return `${y}-${m}-${d} ${h}:${min}:${sec}`;
 };
